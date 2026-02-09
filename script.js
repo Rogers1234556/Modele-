81d8cbc20407bb956d0b1ba5fd04cf6d179446c1
Rogers1234556 <151454593+Rogers1234556@users.noreply.github.com>
2026-02-09 21:04:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -189,8 +189,8 @@ class UIManager {
             const content = document.createElement('div');
             content.className = 'sector-content';
             content.style.position = 'absolute';
-            content.style.top = '7%'; // Сдвигаем выше (было 20%)
-            content.style.left = '50%';
+            content.style.top = '4%'; // Сдвигаем выше (было 20%)
+            content.style.left = '30%';
             content.style.transform = `translateX(-50%) rotate(${angleStep / 2}deg)`;
             content.style.transformOrigin = 'center 135px'; // Поднимаем центр вращения контента
             content.innerHTML = `<i class="fas ${prize.icon}"></i><span>${prize.name}</span>`;
